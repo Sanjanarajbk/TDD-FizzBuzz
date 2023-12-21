90472b8323b6d70b25b8fc6b6ba3eeaae1f4894a
Sanjanarajbk <gowdasanjana60.sg@gmail.com>
2023-12-21 21:18:54 +0530

    [Sanjana] logic to return FizzBuzz for numbers divisible by both 3 and 5

--- src/main/java/Fizzbuzz.java
+++ src/main/java/Fizzbuzz.java
@@ -1,10 +1,10 @@
 public class Fizzbuzz {
-    public String print(int number) {
+    public String convert(int number) {
 
+        if(number % 3 ==0 && number % 5==0) return "FizzBuzz";
         if(number % 3 ==0) return "Fizz";
-        else if (number % 5 ==0) {
-            return "Buzz";
-        }
+        if (number % 5 ==0) return "Buzz";
+
         return null;
     }
 }
--- src/test/java/FizzbuzzTest.java
+++ src/test/java/FizzbuzzTest.java
@@ -5,24 +5,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzbuzzTest {
     @Test
     void shouldReturnFizzIfNumberIsDivisibleBy3() {
-
+       //arrange
         Fizzbuzz fizzbuzz = new Fizzbuzz();
-        String result = fizzbuzz.print(6);
+        //act
+        String result = fizzbuzz.convert(6);
+        //assert
         assertEquals("Fizz",result);
 
     }
 
     @Test
     void ShouldReturnBuzzIfNumberIsDivisibleBy5() {
+
         Fizzbuzz fizzbuzz = new Fizzbuzz();
-        String result = fizzbuzz.print(10);
+
+        String result = fizzbuzz.convert(10);
+
         assertEquals("Buzz",result);
     }
 
     @Test
     void ShouldReturnFizzBuzzIfNumberIsDivisibleByBoth3And5() {
+
         Fizzbuzz fizzbuzz = new Fizzbuzz();
-        String result = fizzbuzz.print(15);
+
+        String result = fizzbuzz.convert(15);
+
         assertEquals("FizzBuzz",result);
     }
 }
