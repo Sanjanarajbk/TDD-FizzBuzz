2a979fdebb784268d5f9773df20e3529f6ef63bd
Sanjanarajbk <gowdasanjana60.sg@gmail.com>
2023-12-21 21:27:20 +0530

    [Sanjana] .Refactor clean code

--- src/main/java/Fizzbuzz.java
+++ src/main/java/Fizzbuzz.java
@@ -1,9 +1,12 @@
 public class Fizzbuzz {
     public String convert(int number) {
 
-        if(isDivisibleBy(number,3) && isDivisibleBy(number,5)) return "FizzBuzz";
-        if(isDivisibleBy(number,3)) return "Fizz";
-        if (isDivisibleBy(number,5)) return "Buzz";
+        if(isDivisibleBy(number,3) && isDivisibleBy(number,5))
+            return "FizzBuzz";
+        if(isDivisibleBy(number,3))
+            return "Fizz";
+        if (isDivisibleBy(number,5))
+            return "Buzz";
 
         return null;
     }
